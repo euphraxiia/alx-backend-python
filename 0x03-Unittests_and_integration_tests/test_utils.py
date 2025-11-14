10b93ee2d2a1d793e3a9b159c6d727079ae4721d
euphraxiia <ST10433587@rcconnect.edu.za>
2025-11-14 20:03:49 +0200

    Fix test_memoize to patch class method before instance creation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -62,9 +62,9 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
+        test_instance = TestClass()
         with patch.object(TestClass, 'a_method',
                           return_value=42) as mock_method:
-            test_instance = TestClass()
             result1 = test_instance.a_property()
             result2 = test_instance.a_property()
 
